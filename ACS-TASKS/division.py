26dbb76d27686b3bb21ac88a5deba898c58486d4
OscarCrosthwaite <crosthwaiteoj@dulwich.org.uk>
2023-09-11 10:17:31 +0100

    Fixed small mistakes

--- ACS-TASKS/division.py
+++ ACS-TASKS/division.py
@@ -2,5 +2,5 @@
 numberOne = int(input("Please choose a number to be divided: "))
 numberTwo = int(input("Please choose a number to be the divider: "))
 valueOne = numberOne//numberTwo
-valueTwo = numberOne // numberTwo
+valueTwo = numberOne % numberTwo
 print(f"The number is {valueOne} and the remainder is {valueTwo}.")
--- ACS-TASKS/options.py
+++ ACS-TASKS/options.py
@@ -1,6 +1,9 @@
 #program that allows user to choose between three options by pressing a number key
-userInput = input("Please select an option between one to three: ")
-if userInput == "1" or userInput == "2" or userInput == "3":
-    print(f"Option {userInput} selected")
-else:
-    print("Invalid option selected")
+while True:
+    userInput = input("Please select an option between one to three: ")
+    if userInput == "1" or userInput == "2" or userInput == "3":
+        print(f"Option {userInput} selected")
+        break
+    else:
+        print("Invalid option selected")
+        continue
