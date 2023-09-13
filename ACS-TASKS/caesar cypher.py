94d9969bc47b8c5148694190d144576cb23e8d39
OscarCrosthwaite <crosthwaiteoj@dulwich.org.uk>
2023-09-13 12:14:28 +0100

    hi

--- ACS-TASKS/caesar cypher.py
+++ ACS-TASKS/caesar cypher.py
@@ -1,6 +1,9 @@
 word = input("Input your word: ")
 
 for i in range(1,len(word) + 1):
-    letter = chr(ord(word[i]) + 3)
-    if letter == 
+    letter = word[i]
+    if letter == "x" or "y" or "z":
+        letter = chr(ord(word[i]) - 23 )
+    else:
+        letter = chr(ord(word[i]) + 3)
     print(letter)
