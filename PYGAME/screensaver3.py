cfa6e2e92e5579249d4022f9f2df3821e09f6e7c
OscarCrosthwaite <crosthwaiteoj@dulwich.org.uk>
2023-10-10 19:14:08 +0100

    finished two player pong (and one player pong)

--- PYGAME/screensaver3.py
+++ PYGAME/screensaver3.py
@@ -24,9 +24,12 @@ y_val = 0
 y_val2 = 0
 
     #ball starting coords
-x_val = 5
-y_val3 = 15
+x_val = 250
+y_val3 = 250
 
+    #lives
+lives1 = 5
+lives2 = 5
 
 #game loop
 done = False
@@ -63,25 +66,67 @@ while not done:
         y_off = -y_off
 
         #collision with rectangles
-    if x_val > 969 and x_val < 1001:
-        if y_val3 > y_val2 -1 and y_val3 < y_val2 + 61:    
+    if x_val > 980 and x_val < 1006:
+        if y_val3 > y_val2 -1 and y_val3 < y_val2 + 91: 
+            #collision effects   
             x_off = -x_off
-    if x_val < 10 and x_val > -25:
-        if y_val3 > y_val -1 and y_val3 < y_val + 61:
+            x_off = x_off * 1.1
+
+
+    if x_val < 9 and x_val > -17:
+        if y_val3 > y_val -1 and y_val3 < y_val + 91:
+            #collision effects
             x_off = -x_off
+            x_off = x_off * 1.1
         
         #consequences of missing balls
-    if x_val < 0 or x_val > 1001:
-        x_val = 25
-        y_val3 = 25
+    if x_val < 0: 
+        x_val = 500
+        y_val3 = 500
+        x_off = 4
+        y_off = 5
+        lives1 -= 1
+        if lives1 == 0:
+            x_val = 500
+            y_val3 = 750
+            x_off = 0
+            y_off = 0
 
+
+
+    if x_val > 1001: 
+        x_val = 500
+        y_val3 = 500
+        x_off = 4
+        y_off = 5
+        lives2 -= 1
+        if lives2 == 0:
+            x_val = 500
+            y_val3 = 750
+            x_off = 0
+            y_off = 0
+            
 
     #drawing code
     screen.fill(BLACK)
-    pygame.draw.rect(screen, WHITE, [x_val, y_val3, 30, 30])
-    pygame.draw.rect(screen, WHITE, [0, y_val, 10, 90])
-    pygame.draw.rect(screen, WHITE, [990, y_val2, 10, 90])
+    pygame.draw.rect(screen, WHITE, [x_val, y_val3, 20, 20])
+    pygame.draw.rect(screen, WHITE, [0, y_val, 13, 90])
+    pygame.draw.rect(screen, WHITE, [990, y_val2, 13, 90])
+    pygame.draw.rect(screen, WHITE, [485, 0, 30, 100])
+
+    #displays lives
+    font = pygame.font.Font(None, 127)
+    score_text1 = font.render(str(lives1), True, RED)
+    screen.blit(score_text1, (425, 5))
+    score_text2 = font.render(str(lives2), True, RED)
+    screen.blit(score_text2, (525, 5))
+    if lives1 == 0:
+        gameLost1 = font.render(str("Player 1 has lost."), True, RED)
+        screen.blit(gameLost1, (50, 100))
     
+    if lives2 == 0:
+            gameLost2 = font.render(str("Player 2 has lost."), True, RED)
+            screen.blit(gameLost2, (50, 100))
 
 
     #game loop
