7794a6b66881c9bc31a09ff2dfceb0c4759a293f
OscarCrosthwaite <crosthwaiteoj@dulwich.org.uk>
2023-10-16 12:14:28 +0100

    fixed movement. started bullet

--- PYGAME/spaceinvaders.py
+++ PYGAME/spaceinvaders.py
@@ -19,26 +19,36 @@ size = (500, 500)
 screen = pygame.display.set_mode(size)
 
 
-pygame.display.set_caption("Let it snow.")
+pygame.display.set_caption("Space Invaders!")
 
 
 done = False
 clock = pygame.time.Clock()
 
 # Classes
-class Player(pygame.sprite.Sprite):
-    #constructor function (or is it?...)
-    def __init__(self, s_width2, s_length2):
+class player(pygame.sprite.Sprite):
+    def __init__(self, s_width, s_length):
         super().__init__()
-        self.image = pygame.Surface([s_width2, s_length2])
+        self.image = pygame.Surface([s_width, s_length])
         self.image.fill(YELLOW)
-        self.speed = 0
         self.rect=self.image.get_rect()
-        self.rect.x = 300
-        self.rect.y = 475
+        self.rect.x = playerX
+        self.rect.y = playerY
     def update(self):
-        self.rect.x = self.rect.x + self.speed
+        self.rect.x = self.rect.x + player_speed
+        
+class Bullets(pygame.sprite.Sprite):
+    def __init__(self, s_width, s_length, s_playerX, s_playerY):
+        super().__init__()
 
+        self.image = pygame.Surface([s_width, s_length])
+        self.image.fill(RED)
+        self.speed = 4
+        self.rect=self.image.get_rect()
+        self.rect.x = s_playerX + 7.5
+        self.rect.y = s_playerY
+    def update(self):
+        self.rect.y = self.rect.y - 2
 
 
 
@@ -50,46 +60,58 @@ class Invaders(pygame.sprite.Sprite):
 
         self.image = pygame.Surface([s_width, s_length])
         self.image.fill(RED)
-        self.speed = random.randrange(1, 4)
+        self.speed = random.randrange(1, 2)
         self.rect=self.image.get_rect()
-        self.rect.x = random.randrange(0, 600)
-        self.rect.y = random.randrange(0, 400)
+        self.rect.x = random.randrange(0, 500)
+        self.rect.y = random.randrange(-50, 0)
     #end of constructor function
     def update(self):
         if self.rect.y > 500:
             self.rect.y = random.randint(0, 50)
-            self.speed = random.randrange(1,4)
+            self.speed = random.randrange(1,2)
         else:
             self.rect.y = self.rect.y + self.speed
 # end Class Snow
 
 # Global Variables
+player_speed = 0
+playerX = 300
+playerY = 450
+printbullet = False
+
 invaders_group = pygame.sprite.Group()
-number_of_aliens = 200
+number_of_aliens = 50
 for i in range(0, number_of_aliens):
     alien = Invaders(10, 10)
     invaders_group.add(alien)
 #next i
 
 player_group = pygame.sprite.Group()
+player_character = player(25, 25)
+player_group.add(player_character)
 
+bullet_group = pygame.sprite.Group()
+bullet = Bullets(10, 10, playerX, playerY)
+bullet_group.add(bullet)
 
 
 while not done:
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT: 
             done =  True 
-        #input keys 
         keys = pygame.key.get_pressed()
-        #player 1
+        player_speed = 0
         if keys[pygame.K_a]:
-            player_speed = -5
+            player_speed = -3
         if keys[pygame.K_d]:
-            player_speed = 5
+            player_speed = 3
+        if keys[pygame.K_w]:
+            printbullet = True
 
     #coding code
     invaders_group.update()
     player_group.update()
+    bullet_group.update()
 
     
     
@@ -97,6 +119,8 @@ while not done:
     screen.fill(BLACK)
     invaders_group.draw(screen)
     player_group.draw(screen)
+    bullet_group.draw(screen)
+    
 
 
 
---
+++ SRC1-TASKS/Data Structures/records.py
@@ -0,0 +1,35 @@
+student1t = ("Dave", "Bloggs", 18, True)
+student2t = ("Fred", "Smith", 16, False)
+
+
+class Student_record:
+    def __init__(self):
+        self.firstname = ""
+        self.lastname= ""
+        self.age = 0
+        self.registered = False
+    #end constructor
+#end class
+
+student1 = Student_record()
+student1.firstname = "Dave"
+student1.lastname = "Bloggs"
+student1.age = 18
+student1.registered = True
+
+
+student2 = Student_record()
+student2.firstname = "Fred"
+student2.lastname = "Smith"
+student2.age = 16
+student2.registered = False
+
+print(student1t[2])
+
+def addone(x):
+    x = x + 1
+    return x
+#endfunction
+
+newage = addone(student2.age)
+print(newage)
